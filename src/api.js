c9df501eec31db8a87e6987c21a7846f5643824d
Shane <6071159+smashedr@users.noreply.github.com>
2025-10-11 23:29:26 -0700

    Update Cache

--- src/api.js
+++ src/api.js
@@ -192,7 +192,7 @@ export async function getVTStats(sha, id = false) {
     // const stats = report?.data?.attributes?.last_analysis_stats
     // console.log('stats:', stats)
     if (!stats) await cacheError(key, 'VT Stats Not Found')
-    await cacheSet(key, stats, 60 * 24 * 2)
+    await cacheSet(key, stats, 60 * 60 * 24)
     return stats
 }
 
