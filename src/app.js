4bba7f7a53156b2ae36f218d4b103ed422e5c949
Shane <6071159+smashedr@users.noreply.github.com>
2025-10-11 17:55:20 -0700

    Cleanup

--- src/app.js
+++ src/app.js
@@ -422,7 +422,7 @@ function getRangedColor(req, index, options = {}) {
         .mode('lab')
         .colors(opts.total + 1)
     // console.log('colors:', colors)
-    colors.forEach((color) => console.log(color))
+    // colors.forEach((color) => console.log(color))
     const idx = Math.max(0, Math.min(opts.total, index))
     console.log(`index: ${idx} / ${colors.length - 1}`)
     return colors[idx]
