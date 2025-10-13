1fe9fe82d378a91c1f6492e689e45e3329e4b5ed
Shane <6071159+smashedr@users.noreply.github.com>
2025-10-12 23:17:43 -0700

    Update Sentry Options

--- src/instrument.js
+++ src/instrument.js
@@ -1,11 +1,11 @@
 if (process.env.SENTRY_URL) {
     const environment = process.env.SENTRY_ENVIRONMENT || 'dev'
     console.log(`SENTRY_ENVIRONMENT: ${environment}`)
-    console.log(`SENTRY_URL: ${process.env.SENTRY_URL.substring(0, 16)}...`)
+    console.log(`SENTRY_URL: ${process.env.SENTRY_URL.substring(0, 14)}...`)
     const Sentry = await import('@sentry/node')
+    // https://docs.sentry.io/platforms/javascript/configuration/options/
     Sentry.init({
         dsn: process.env.SENTRY_URL,
-        sendDefaultPii: true,
         environment,
     })
 } else {
