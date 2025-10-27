fa7644b0b916b1e042a4ee627fa4b21c51ace8e2
Shane <6071159+smashedr@users.noreply.github.com>
2025-10-27 00:57:14 -0700

    Add debug Package

--- src/github.js
+++ src/github.js
@@ -1,4 +1,7 @@
+import createDebug from 'debug'
 import { Octokit } from '@octokit/rest'
+
+export const debug = createDebug('app:api')
 
 export class GitHubApi {
     /**
@@ -17,12 +20,12 @@ export class GitHubApi {
     //  * @return {Promise<Object>}
     //  */
     // async getRelease(release_id) {
-    //     console.log('getRelease:', release_id)
+    //     debug('getRelease:', release_id)
     //     const response = await this.octokit.rest.repos.getRelease({
     //         ...github.context.repo,
     //         release_id,
     //     })
-    //     console.log('response.status:', response.status)
+    //     debug('response.status:', response.status)
     //     return response.data
     // }
 
@@ -34,13 +37,13 @@ export class GitHubApi {
      * @return {Promise<Object>}
      */
     async getReleaseByTag(owner, repo, tag) {
-        console.log('getReleaseByTag:', tag)
+        debug('getReleaseByTag:', tag)
         const response = await this.octokit.rest.repos.getReleaseByTag({
             owner,
             repo,
             tag,
         })
-        console.log('response.status:', response.status)
+        debug('response.status:', response.status)
         return response.data
     }
 
@@ -51,16 +54,16 @@ export class GitHubApi {
      * @return {Promise<Object|Undefined>}
      */
     async getLatestRelease(owner, repo) {
-        console.log('getLatestRelease:', owner, repo)
+        debug('getLatestRelease:', owner, repo)
         try {
             const response = await this.octokit.rest.repos.getLatestRelease({
                 owner,
                 repo,
             })
-            console.log('response.status:', response.status)
+            debug('response.status:', response.status)
             return response.data
         } catch (error) {
-            console.log('error:', error)
+            console.error('error:', error)
         }
     }
 }
