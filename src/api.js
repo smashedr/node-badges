01059a45100a0d0ea804c72aa01b077e00234dc5
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2026-06-13 11:03:47 -0700

    Fix Multi-Arch Image Size (#54)

--- src/api.js
+++ src/api.js
@@ -82,31 +82,35 @@ export class GHCRApi {
     const indexManifest = await this.getManifest(tag)
     debug('mediaType:', indexManifest.mediaType)
 
-    let totalSize = 0
-
     if (
       !indexManifest.mediaType.includes('list') &&
       !indexManifest.mediaType.includes('index')
     ) {
       // debug('indexManifest - !list + !index:', indexManifest)
+      // noinspection JSUnresolvedReference
       const size = indexManifest.layers.reduce((sum, layer) => sum + layer.size, 0)
-      totalSize = size + (indexManifest.config.size || 0)
+      const totalSize = size + (indexManifest.config.size || 0)
       // debug('totalSize:', totalSize)
       await cacheSet(key, totalSize)
       return totalSize
     }
 
     debug('indexManifest.manifests?.length:', indexManifest.manifests?.length)
-    for (const m of indexManifest.manifests) {
-      await new Promise((resolve) => setTimeout(resolve, 50))
-      const manifest = await this.getManifest(m.digest)
-      const configSize = manifest.config?.size || 0
-      // debug('configSize:', configSize)
-      // noinspection JSUnresolvedReference
-      const layerSize = manifest.layers?.reduce((a, l) => a + (l.size || 0), 0) || 0
-      // debug('layerSize:', layerSize)
-      totalSize += configSize + layerSize
-    }
+    // only process linux/amd64 or first item...
+    // noinspection JSValidateTypes
+    const m =
+      indexManifest.manifests?.find(
+        (m) => m.platform?.os === 'linux' && m.platform?.architecture === 'amd64',
+      ) || indexManifest.manifests?.[0]
+    debug('indexManifest:', m)
+    await new Promise((resolve) => setTimeout(resolve, 50))
+    const manifest = await this.getManifest(m.digest)
+    const configSize = manifest.config?.size || 0
+    // debug('configSize:', configSize)
+    // noinspection JSUnresolvedReference
+    const layerSize = manifest.layers?.reduce((a, l) => a + (l.size || 0), 0) || 0
+    // debug('layerSize:', layerSize)
+    const totalSize = configSize + layerSize
     // debug('totalSize:', totalSize)
     await cacheSet(key, totalSize, 60 * 60 * 4)
     return totalSize
